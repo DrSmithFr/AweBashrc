c16a8d9bdb41b2329c79a45632ba131c5bd792e2
John D <johndaligault@gmail.com>
2022-11-13 11:48:41 +0100

    updating install script

--- install.sh
+++ install.sh
@@ -7,13 +7,6 @@ grep -vwE ". /home/john/Programmes/AweBash/Awe.sh" ~/.bashrc.bak > ~/.bashrc
 echo ". $AWE_INSTALL_FOLDER/Awe.sh" >> ~/.bashrc
 echo -e "Updating your .bashrs ${BGreen}OK${NC}"
 
-git clone https://github.com/MrSmith777/AweCD plugins/AweCD
-git clone https://github.com/MrSmith777/AweGit plugins/AweGit
-git clone https://github.com/MrSmith777/AweDocker plugins/AweDocker
-git clone https://github.com/MrSmith777/AweSymfony plugins/AweSymfony
-git clone https://github.com/MrSmith777/AweSystemInfo plugins/AweSystemInfo
-git clone https://github.com/MrSmith777/AweTheFuck plugins/AweTheFuck
-
 export AWE_DEBUG="TRUE"
 . "$AWE_INSTALL_FOLDER/Awe.sh"
 export AWE_DEBUG=""
