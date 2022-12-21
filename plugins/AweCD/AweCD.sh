161b5fa7661901d5c1ae5cc9a112f431ae4f3322
John D <johndaligault@gmail.com>
2022-12-21 13:09:03 +0100

    Adding symfony alias

--- plugins/AweCD/AweCD.sh
+++ plugins/AweCD/AweCD.sh
@@ -1,5 +1,50 @@
 #!/usr/bin/env bash
 
+# Adding nvm
+cdnvm() {
+    command cd "$@" || return $?
+    nvm_path=$(nvm_find_up .nvmrc | tr -d '\n')
+
+    # If there are no .nvmrc file, use the default nvm version
+    if [[ ! $nvm_path = *[^[:space:]]* ]]; then
+
+        declare default_version;
+        default_version=$(nvm version default);
+
+        # If there is no default version, set it to `node`
+        # This will use the latest version on your machine
+        if [[ $default_version == "N/A" ]]; then
+            nvm alias default node;
+            default_version=$(nvm version default);
+        fi
+
+        # If the current version is not the default version, set it to use the default version
+        if [[ $(nvm current) != "$default_version" ]]; then
+            nvm use default;
+        fi
+
+    elif [[ -s $nvm_path/.nvmrc && -r $nvm_path/.nvmrc ]]; then
+        declare nvm_version
+        nvm_version=$(<"$nvm_path"/.nvmrc)
+
+        declare locally_resolved_nvm_version
+        # `nvm ls` will check all locally-available versions
+        # If there are multiple matching versions, take the latest one
+        # Remove the `->` and `*` characters and spaces
+        # `locally_resolved_nvm_version` will be `N/A` if no local versions are found
+        locally_resolved_nvm_version=$(nvm ls --no-colors "$nvm_version" | tail -1 | tr -d '\->*' | tr -d '[:space:]')
+
+        # If it is not already installed, install it
+        # `nvm install` will implicitly use the newly-installed version
+        if [[ "$locally_resolved_nvm_version" == "N/A" ]]; then
+            nvm install "$nvm_version";
+        elif [[ $(nvm current) != "$locally_resolved_nvm_version" ]]; then
+            nvm use "$nvm_version";
+        fi
+    fi
+}
+
+# CD with restoration of PWD
 actual=$(pwd)
 AWE_EXT_AWECD="$AWE_PLUGIN_CURRENT_CACHE_FOLDER/$USER"
 
@@ -27,7 +72,7 @@ case $opt in
     --restore|-r)
             lastdir="$(cat $AWE_EXT_AWECD/.lastdir)">/dev/null 2>&1
             if [ -d "$lastdir" ]; then
-                command cd "$lastdir"
+                cdnvm "$lastdir"
                 export PWD=$(pwd);
             else
                 echo -e "bash: cd: nothing to restore"
@@ -46,7 +91,7 @@ case $opt in
         then
             lastdir="$(cat $AWE_EXT_AWECD/.lastdir_$opt)">/dev/null 2>&1
             if [ -d "$lastdir" ]; then
-                command cd "$lastdir"
+                cdnvm "$lastdir"
                 export PWD=$(pwd);
                 bash_save_last_pwd
             else
@@ -61,13 +106,13 @@ case $opt in
     *)
         if [ -z "$opt" ]
         then
-            command cd "$HOME" && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
+            cdnvm "$HOME" && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
             export PWD=$(pwd);
             bash_save_last_pwd
         else
             if [ -d "$opt" ]
             then
-                command cd $opt && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
+                cdnvm $opt && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
                 export PWD=$(pwd);
                 bash_save_last_pwd
             else
@@ -75,7 +120,7 @@ case $opt in
                 then
                     lastdir="$(cat $AWE_EXT_AWECD/.lastdir_$opt)">/dev/null 2>&1
                     if [ -d "$lastdir" ]; then
-                        command cd "$lastdir" && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
+                        cdnvm "$lastdir" && ls -h --color -lv --group-directories-first | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print $0}'
                         export PWD=$(pwd);
                         bash_save_last_pwd
                     else
---
+++ plugins/AweSymfony/Loader.awe.sh
@@ -0,0 +1,5 @@
+# Symfony
+alias composer="symfony composer"
+alias php="symfony php"
+alias phpunit="symfony php bin/phpunit"
+alias phpunit-coverage="symfony php bin/phpunit --coverage-html coverage"
