f48dedb9a2b41301c175f92f8e8cab32a083805d
John D <johndaligault@gmail.com>
2023-01-21 18:04:37 +0100

    Removing politics from composer

--- plugins/AweSymfony/Loader.awe.sh
+++ plugins/AweSymfony/Loader.awe.sh
@@ -1,5 +1,9 @@
 # Symfony
-alias composer="symfony composer"
+
+#alias composer="symfony composer"
+#Removing politics from the composer.
+alias composer="2> >(grep -av 'StandWith') symfony composer --ansi "
+
 alias php="symfony php"
 alias phpunit="symfony php bin/phpunit"
 alias phpunit-coverage="symfony php bin/phpunit --coverage-html coverage"
