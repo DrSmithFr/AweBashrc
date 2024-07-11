2ccf08af6f923cd069f88e9a26a516750d652ee8
John.D <johndaligault@gmail.com>
2024-07-11 13:58:48 +0200

    Adding JetBrain Toolscript to the global path

--- core/config/Path.sh
+++ core/config/Path.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-PATH=$PATH:/usr/local:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/cuda-8.0/bin:$HOME/.cargo/env
+PATH=$PATH:/usr/local
+PATH=$PATH:/usr/local/sbin
+PATH=$PATH:/usr/local/bin
+PATH=$PATH:/usr/sbin
+PATH=$PATH:/usr/bin
+PATH=$PATH:/sbin
+PATH=$PATH:/bin
+PATH=$PATH:/usr/games
+PATH=$PATH:/usr/local/cuda-8.0/bin
+PATH=$PATH:$HOME/.cargo/env
+PATH=$PATH:/home/john/.local/share/JetBrains/Toolbox/scripts
 
-# remove duplicate path entries
-export PATH=$(echo $PATH | awk -F: '
+# Remove duplicates
+CLEAN_PATH=$(echo $PATH | awk -F: '
 { for (i = 1; i <= NF; i++) arr[$i]; }
 END { for (i in arr) printf "%s:" , i; printf "\n"; } ')
 
+# Update PATH variable
+if [ "$CLEAN_PATH" != "" ]; then
+  export PATH=$CLEAN_PATH
+else
+  export PATH
+fi
+
+# Export CUDA library path
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
